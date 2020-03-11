a0e3b7d1271f415d38305ba2037922d9f6ef8bd1
ypb <obpt@sina.com>
2020-03-12 00:18:05 +0800

    消除重复System.out.println,使用自定义函数,方便替换

--- src/main/java/cn/xpbootcamp/bugszero/Game.java
+++ src/main/java/cn/xpbootcamp/bugszero/Game.java
@@ -44,8 +44,8 @@ public class Game {
 	    purses[howManyPlayers()] = 0;
 	    inPenaltyBox[howManyPlayers()] = false;
 
-	    System.out.println(playerName + " was added");
-	    System.out.println("They are player number " + players.size());
+	    logInfo(playerName + " was added");
+	    logInfo("They are player number " + players.size());
 		return true;
 	}
 
@@ -54,18 +54,18 @@ public class Game {
 	}
 
 	public void roll(int roll) {
-		System.out.println(players.get(currentPlayer) + " is the current player");
-		System.out.println("They have rolled a " + roll);
+		logInfo(players.get(currentPlayer) + " is the current player");
+		logInfo("They have rolled a " + roll);
 
 		if (inPenaltyBox[currentPlayer]) {
 			if (roll % 2 != 0) {
 				isGettingOutOfPenaltyBox = true;
 
-				System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
+				logInfo(players.get(currentPlayer) + " is getting out of the penalty box");
 				inPenaltyBox[currentPlayer] = false;
 				movePlayerAndAskQuestion(roll);
 			} else {
-				System.out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
+				logInfo(players.get(currentPlayer) + " is not getting out of the penalty box");
 				isGettingOutOfPenaltyBox = false;
 				}
 
@@ -83,22 +83,22 @@ public class Game {
 		places[currentPlayer] = places[currentPlayer] + roll;
 		if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
 
-		System.out.println(players.get(currentPlayer)
+		logInfo(players.get(currentPlayer)
                 + "'s new location is "
                 + places[currentPlayer]);
-		System.out.println("The category is " + currentCategory());
+		logInfo("The category is " + currentCategory());
 		askQuestion();
 	}
 
 	private void askQuestion() {
 		if (currentCategory() == "Pop")
-			System.out.println(popQuestions.removeFirst());
+			logInfo(popQuestions.removeFirst());
 		if (currentCategory() == "Science")
-			System.out.println(scienceQuestions.removeFirst());
+			logInfo(scienceQuestions.removeFirst());
 		if (currentCategory() == "Sports")
-			System.out.println(sportsQuestions.removeFirst());
+			logInfo(sportsQuestions.removeFirst());
 		if (currentCategory() == "Rock")
-			System.out.println(rockQuestions.removeFirst());
+			logInfo(rockQuestions.removeFirst());
 	}
 
 
@@ -118,9 +118,9 @@ public class Game {
 	public boolean wasCorrectlyAnswered() {
 		if (inPenaltyBox[currentPlayer]){
 			if (isGettingOutOfPenaltyBox) {
-				System.out.println("Answer was correct!!!!");
+				logInfo("Answer was correct!!!!");
 				purses[currentPlayer]++;
-				System.out.println(players.get(currentPlayer)
+				logInfo(players.get(currentPlayer)
 						+ " now has "
 						+ purses[currentPlayer]
 						+ " Gold Coins.");
@@ -136,9 +136,9 @@ public class Game {
 
 		} else {
 
-			System.out.println("Answer was correct!!!!");
+			logInfo("Answer was correct!!!!");
 			purses[currentPlayer]++;
-			System.out.println(players.get(currentPlayer)
+			logInfo(players.get(currentPlayer)
 					+ " now has "
 					+ purses[currentPlayer]
 					+ " Gold Coins.");
@@ -155,8 +155,8 @@ public class Game {
 	}
 
 	public boolean wrongAnswer(){
-		System.out.println("Question was incorrectly answered");
-		System.out.println(players.get(currentPlayer)+ " was sent to the penalty box");
+		logInfo("Question was incorrectly answered");
+		logInfo(players.get(currentPlayer)+ " was sent to the penalty box");
 		inPenaltyBox[currentPlayer] = true;
 
 		return true;
@@ -165,5 +165,9 @@ public class Game {
 
 	private boolean didPlayerWin() {
 		return !(purses[currentPlayer] == 6);
+	}
+
+	private void logInfo(Object message){
+		System.out.println(message);
 	}
 }
